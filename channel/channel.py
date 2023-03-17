9124b96733fc59d9e0d51717138bfb9bfa60550e
João Pedro Lourenço Affonso <56265673+joaopedrolourencoaffonso@users.noreply.github.com>
2023-03-17 08:03:13 -0300

    Novo channel.py
    Nova versao incluí:
    - Função para listar todos os diálogos que o usuário faz parte
    - Opção de escolher o canal do qual você quer pegar informações pela linha de comando
    - Opção de selecionar pela linha de comando o id a partir do qual você quer extrair estatísticas
    ------------------------------------------
    New version includes:
    - Function to list all dialogs that the user is part of
    - Option to choose the channel you want to get information from the command line
    - Option to select from the command line the id from which you want to extract statistics

--- channel/channel.py
+++ channel/channel.py
@@ -12,25 +12,36 @@ api_hash = "API_HASH";
 #logging
 import logging
 log_format = "%(levelname)s ;; %(asctime)s ;; %(message)s";
-logging.basicConfig(level=logging.INFO,filename="quotes_bot.log",format=log_format);
+logging.basicConfig(level=logging.INFO,filename="channel.log",format=log_format);
 logger = logging.getLogger();
+#Permite exportar para arquivos pelo ">"
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
 
-async def main():
+client = TelegramClient('bot', api_id, api_hash)
+
+async def lista_dialogos():
     try:
-        #ultimo id recebido
-        try:
-            with open("stats.csv", "r") as file:
-                last_id = file.readlines()[-1];
-                last_id = int(last_id.split(",")[1]);
-                
-        except:
-            last_id = 1;
+        print("+---------------------+---------------------+")
+        async for dialog in client.iter_dialogs():
+            print("|{:<20} | {:<20}|".format(dialog.name,dialog.id));
+
+        print("+---------------------+---------------------+")
+
+    except Exception as e:
+        print("Erro, por favor, veja os logs");
+        logger.error(f" - lista_dialogos - " + str(e));
+
+async def estatisticas(channel_id, last_id):
+    try:
+        if not last_id:
+            last_id = 1
         
         #pegando todas as mensagens desde o ultimo id
-        async for message in client.iter_messages("id_do_canal_do_telegram", reverse=True, min_id=last_id):
+        async for message in client.iter_messages(int(channel_id), reverse=True, min_id=int(last_id)):
             if message.reactions is None:
-                #timedelta for correcting to my timezone
-                registro = f"\n{message.date - timedelta(hours=3)}, {message.id}, {message.views}";
+                #timedelta for correcting to my timezone (Brasilia)
+                registro = f"{message.date - timedelta(hours=3)}, {message.id}, {message.views}";
 
             else:
                 temp = ""
@@ -39,16 +50,28 @@ async def main():
                     count = reaction.count;
                     temp = temp + f"{emoticon}|{count} - ";
 
-                registro = f"\n{message.date - timedelta(hours=3)}, {message.id}, {message.views},{temp}";
+                registro = f"{message.date - timedelta(hours=3)}, {message.id}, {message.views},{temp}";
 
-            open("stats.csv","a",encoding='utf-8').write(registro)
+            print(registro)
 
     except Exception as e:
         print("Erro, por favor, veja os logs");
         logger.error(f" - main - " + str(e));
 
-parser = argparse.ArgumentParser(description='Pegando estatísticas do canal');
-        
+parser = argparse.ArgumentParser(description='Um script para extrair estatísticas sobre canais do Telegram.');
+parser.add_argument('--dialogos',action="store_true",default=False, help='Gera uma lista com todos os diálogos do usuário.');
+parser.add_argument('--channel',default=False, help='Retorna estatísticas sobre um canal.');
+parser.add_argument('--last_id',default=False, help='Usado em associacao a "--channel" retorna mensagens com id superior ao especificado, ou seja, desde aquele ponto.');
+
+args = parser.parse_args();
+
 if __name__ == '__main__':
     with client:
-        client.loop.run_until_complete(main())
+        if args.dialogos:
+            client.loop.run_until_complete(lista_dialogos())
+        
+        if args.channel:
+            client.loop.run_until_complete(estatisticas(args.channel, args.last_id));
+
+        if not (args.dialogos or args.channel):
+            print("E necessario passar argumentos, verifique a funcao help")
